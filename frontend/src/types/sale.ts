6c1e247763953a2eb1567eddf65bff3da7bb1501
Lohan <lohanamendola18@gmail.com>
2021-11-07 18:29:34 -0400

    URL HEROKU

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,15 @@
+import { Seller } from "./seller"
+
+export type Sale ={
+
+    id: number;
+    visited: number,
+    deals: number;
+    amount: number;
+    date: string,
+    seller: Seller;  
+}          
+
 export type SaleSum = {
 
     sellerName: string;
