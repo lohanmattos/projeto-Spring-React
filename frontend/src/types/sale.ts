05a560af1db3b47732a79e2996d512d904413184
Lohan <lohanamendola18@gmail.com>
2021-11-07 18:59:46 -0400

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -10,6 +10,19 @@ export type Sale ={
     seller: Seller;  
 }          
 
+export type SalePage ={
+
+    content?: Sale[];
+    last: boolean;
+    totalElements: number;
+    totalPages: number;
+    size?: number;
+    number: number;
+    first: boolean;
+    numberOfElements?: number;
+    empty?: boolean;
+}
+
 export type SaleSum = {
 
     sellerName: string;
